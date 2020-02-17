c7eae3c16a1ac5b9100f714e1523165b40fd877c
Jesse Mazzella <jessemazzella@gmail.com>
2020-02-17 13:41:20 -0800

    Refactor: use match statement and Display trait

--- src/bin/wc.rs
+++ src/bin/wc.rs
@@ -1,6 +1,7 @@
 extern crate regex;
 use std::env;
 use std::error::Error;
+use std::fmt;
 use std::fs;
 use std::process;
 
@@ -27,20 +28,44 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-pub fn count(config: Config) -> Result<String, Box<dyn Error>> {
+pub fn count(config: Config) -> Result<FileStats, Box<dyn Error>> {
     let contents = fs::read_to_string(&config.filename)?;
     match config.mode {
-        CountMode::Bytes => Ok(format!("{}\t{}", contents.bytes().count(), config.filename)),
-        CountMode::Lines => Ok(format!("{}\t{}", contents.lines().count(), config.filename)),
-        CountMode::Characters => Ok(format!("{}\t{}", contents.chars().count(), config.filename)),
-        CountMode::Words => Ok(format!("{}\t{}", count_words(&contents), config.filename)),
-        CountMode::All => Ok(format!(
-            "{}\t{}\t{}\t{}",
-            contents.lines().count(),
-            count_words(&contents),
-            contents.chars().count(),
-            config.filename
-        )),
+        CountMode::Bytes => Ok(FileStats {
+            bytes: Some(contents.bytes().count()),
+            lines: None,
+            chars: None,
+            words: None,
+            filename: config.filename,
+        }),
+        CountMode::Lines => Ok(FileStats {
+            bytes: None,
+            lines: Some(contents.lines().count()),
+            chars: None,
+            words: None,
+            filename: config.filename,
+        }),
+        CountMode::Characters => Ok(FileStats {
+            bytes: None,
+            lines: None,
+            chars: Some(contents.chars().count()),
+            words: None,
+            filename: config.filename,
+        }),
+        CountMode::Words => Ok(FileStats {
+            bytes: None,
+            lines: None,
+            chars: None,
+            words: Some(count_words(&contents)),
+            filename: config.filename,
+        }),
+        CountMode::All => Ok(FileStats {
+            bytes: None,
+            lines: Some(contents.lines().count()),
+            chars: Some(contents.chars().count()),
+            words: Some(count_words(&contents)),
+            filename: config.filename,
+        }),
     }
 }
 
@@ -48,12 +73,52 @@ pub fn count_words<'a>(contents: &'a str) -> usize {
     let mut count: usize = 0;
 
     for line in contents.lines() {
-        for _word in line.split_whitespace() {
-            count += 1;
-        }
+        count += line.split_whitespace().count();
     }
 
     count
+}
+
+pub struct FileStats {
+    filename: String,
+    bytes: Option<usize>,
+    lines: Option<usize>,
+    chars: Option<usize>,
+    words: Option<usize>,
+}
+
+impl Default for FileStats {
+    fn default() -> Self {
+        Self {
+            filename: String::new(),
+            bytes: None,
+            lines: None,
+            chars: None,
+            words: None,
+        }
+    }
+}
+
+// To use the `{}` marker, the trait `fmt::Display` must be implemented
+// manually for the type.
+impl fmt::Display for FileStats {
+    // This trait requires `fmt` with this exact signature.
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Write strictly the first element into the supplied output
+        // stream: `f`. Returns `fmt::Result` which indicates whether the
+        // operation succeeded or failed. Note that `write!` uses syntax which
+        // is very similar to `println!`.
+        match (self.bytes, self.lines, self.chars, self.words) {
+            (Some(bytes), None, None, None) => write!(f, "{}\t{}", bytes, self.filename),
+            (None, Some(lines), None, None) => write!(f, "{}\t{}", lines, self.filename),
+            (None, None, Some(chars), None) => write!(f, "{}\t{}", chars, self.filename),
+            (None, None, None, Some(words)) => write!(f, "{}\t{}", words, self.filename),
+            (None, Some(lines), Some(chars), Some(words)) => {
+                write!(f, "{}\t{}\t{}\t{}", lines, words, chars, self.filename)
+            }
+            (_, _, _, _) => panic!("This should never happen! Contact the developer immediately"),
+        }
+    }
 }
 
 pub enum CountMode {
